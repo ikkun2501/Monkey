93def6509fd6fea7b7bfcff0095b42a6ac4bfb3a
mori <mori@casareal.co.jp>
2018-10-18 12:51:15 +0900

    2.8.3 if式

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -37,6 +37,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 
 	// 中置演算子Parserの関数を登録
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
@@ -328,4 +329,74 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 		return nil
 	}
 	return exp
+}
+
+// if式
+func (p *Parser) parseIfExpression() ast.Expression {
+
+	// if tokenの設定
+	expression := &ast.IfExpression{Token: p.curToken}
+
+	// 次のTOKENがLPARENでなければnil
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	// 次のTOKEN
+	p.nextToken()
+
+	// 条件
+	expression.Condition = p.parseExpression(LOWEST)
+
+	// 次のTOKENがRPARENでなければnil
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	// 次のTOKENがLBRACEでなければnil
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	// ブロック文
+	expression.Consequence = p.parseBlockStatement()
+
+	// ELSE TOKENがあれば
+	if p.peekTokenIs(token.ELSE) {
+
+		//
+		p.nextToken()
+
+		// LBRACEがなければ
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+
+		// ブロック文を取得
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
+
+// ブロック文のparse
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+
+	// curToken "{"
+	fmt.Printf("BlockStatement curToken:%#v\n", p.curToken)
+	block := &ast.BlockStatement{Token: p.curToken}
+
+	// 式の配列
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+	// RBRACE"}"かEOFになるまで式を読み取る
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return block
 }
