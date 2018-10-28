c1195c68095c99e7c3a67fa61f935d4d9535bd65
ikkun2501 <ikkun2501@gmail.com>
2018-10-28 11:37:27 +0900

    3.7 return文

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -17,7 +17,10 @@ func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
+
+	case *ast.BlockStatement:
+		return evalBlockStatement(node)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -43,6 +46,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IfExpression:
 		return evalIfExpression(node)
 
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
+
 	}
 	return nil
 }
@@ -53,6 +60,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 	for _, statement := range stmts {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 	return result
 }
@@ -166,4 +177,33 @@ func isTruthy(obj object.Object) bool {
 	default:
 		return true
 	}
+}
+
+// Programの評価
+func evalProgram(program *ast.Program) object.Object {
+	var result object.Object
+
+	for _, statement := range program.Statements {
+		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+	return result
+}
+
+// 文の評価
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+	return result
 }
