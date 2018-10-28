ce8a1800db8068927fe8ee5c4f76dc1f416c1fbe
ikkun2501 <ikkun2501@gmail.com>
2018-10-28 23:22:08 +0900

    4.4.4 添字リテラルの評価

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -18,6 +18,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BULTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type Object interface {
@@ -124,3 +125,23 @@ type Builtin struct {
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, " ,"))
+	out.WriteString("]")
+
+	return out.String()
+}
